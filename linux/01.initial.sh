c93f0b8a0dd9111f93ef9b734b53e24cf97f099c
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-25 12:50:40 +0100

    Update 01.initial.sh

--- linux/01.initial.sh
+++ linux/01.initial.sh
@@ -1,7 +1,7 @@
 apt update
 apt -y full-upgrade
 apt autoremove
-apt install -y ufw openvpn openresolv vim lynx lynis rkhunter apt-listbugs needrestart fail2ban clamav clamav-daemon
+apt install -y ufw openvpn openresolv vim lynx lynis rkhunter apt-listbugs needrestart fail2ban clamav clamav-daemon nginx
 
 wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
 chmod +x "/etc/openvpn/update-resolv-conf"
