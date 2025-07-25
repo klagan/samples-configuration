8265684ac5f56bd675fadb8e08e829962351d6f8
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-25 16:33:13 +0100

    Update 01-installations.sh

--- linux/01-installations.sh
+++ linux/01-installations.sh
@@ -3,6 +3,7 @@ apt -y full-upgrade
 apt autoremove
 apt install -y ufw openvpn openresolv vim lynx lynis rkhunter apt-listbugs needrestart fail2ban clamav clamav-daemon nginx docker.io
 
+# proton specific configuration of AOVPN
 wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
 chmod +x "/etc/openvpn/update-resolv-conf"
 
