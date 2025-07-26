eef5671ac7d0213a086c2a0bbf3e1b9ab2a24d5a
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-27 00:57:16 +0100

    Update configure.sh

--- linux/calibre-web/configure.sh
+++ linux/calibre-web/configure.sh
@@ -1,10 +1,3 @@
-# create user
-useradd -m calibreweb
-
-# create folders
-mkdir /calibreweb /calibreweb/books /calibreweb/db /calibreweb/config
-chown -R $(id -u calibreweb):$(id -g calibreweb) /calibreweb
-
 # set environment variables
 export CIFS_SERVER=192.168.0.0
 export CIFS_SHARE=Public/Books
@@ -13,4 +6,12 @@ export CREDENTIALS_FILE=/calibreweb/.credentials
 export OWNER_NAME=calibreweb
 export OWNER_GROUP_NAME=calibreweb
 
+# create user
+useradd -m ${OWNER_NAME}
+
+# create folders
+mkdir /${OWNER_NAME} /${OWNER_NAME}/books /${OWNER_NAME}/db /${OWNER_NAME}/config
+chown -R $(id -u ${OWNER_NAME}):$(id -g ${OWNER_NAME}) /${OWNER_NAME}
+
+# map drives
 ./map-drive.sh
