cea61d2e6c46dd44a07907052db2a2a90e049c30
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-26 12:33:47 +0100

    Update 01-installations.sh

--- linux/01-installations.sh
+++ linux/01-installations.sh
@@ -28,7 +28,8 @@ sudo sysctl -w vm.swappiness=10
 sudo sysctl -w vm.vfs_cache_pressure=50
 
 # verify installations
-docker -v
+docker version
+docker compose version
 nginx -v
 systemctl status nginx
 sudo systemctl list-units --type=service --state=running
