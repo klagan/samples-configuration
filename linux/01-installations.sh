84d51f68a77bbb18f08a3d338eebd3a91272f732
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-26 12:17:18 +0100

    Update 01-installations.sh

--- linux/01-installations.sh
+++ linux/01-installations.sh
@@ -1,7 +1,7 @@
 apt update
 apt -y full-upgrade
 apt autoremove
-apt install -y ufw openvpn openresolv vim lynx lynis rkhunter apt-listbugs needrestart fail2ban clamav clamav-daemon nginx docker.io samba-client netdiscover cifs-utils nfs-common
+apt install -y ufw openvpn openresolv vim lynx lynis rkhunter apt-listbugs needrestart fail2ban clamav clamav-daemon nginx docker.io samba-client netdiscover cifs-utils nfs-common ca-certificates curl gnupg lsb-release
 
 # proton specific configuration of AOVPN
 wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
