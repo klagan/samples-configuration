8d523355db21cb719555413dee56ebd019c8d5d6
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-27 17:00:19 +0100

    Update install.sh

--- linux/vpn/protonVPN/install.sh
+++ linux/vpn/protonVPN/install.sh
@@ -16,7 +16,7 @@ echo "Configure OpenVPN: sudo openvpn <config.ovpn>"
 export vpn_username=
 export vpn_password=
 export credentials_file=.vpn_credentials
-export ovpn_filename=ch.protonvpn.udp.ovpn
+export ovpn_filename=/root/ch.protonvpn.udp.ovpn
 
 echo "${vpn_username}" | tee "${credentials_file}" > /dev/null
 echo "${vpn_password}" | tee -a "${credentials_file}" > /dev/null
@@ -27,10 +27,11 @@ chmod 600 /etc/openvpn/${credentials_file}
 # configure vpn
 cp ${ovpn_filename} /etc/openvpn/myvpn.conf
 
-echo "EDIT THIS LINE IN /etc/openvpn/myvpn.conf : auth-user-pass /etc/openvpn/.vpn_credentials"
-echo "ENABLE SERVICE: systemctl enable openvpn@myvpn.service"
-echo "START SERVICE: systemctl start openvpn@myvpn.service"
-echo "CHECK SERVICE: systemctl status openvpn@myvpn.service"
+echo -e "\e[33mEDIT THIS LINE IN /etc/openvpn/myvpn.conf : auth-user-pass /etc/openvpn/.vpn_credentials\e[0m"
+echo -e "\e[33mENABLE SERVICE: systemctl enable openvpn@myvpn.service\e[0m"
+echo -e "\e[33mSTART SERVICE: systemctl start openvpn@myvpn.service\e[0m"
+echo -e "\e[33mCHECK SERVICE: systemctl status openvpn@myvpn.service\e[0m"
+echo -e "\e[33mVALIDATE: curl ipinfo.io\e[0m"
 
 # troubleshooting
 # journalctl -u openvpn@myvpn.service -f
