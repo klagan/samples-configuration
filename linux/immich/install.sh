11ebee63878f315f237ce590927fb9b238aa1720
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-28 18:53:47 +0100

    Create install.sh

---
+++ linux/immich/install.sh
@@ -0,0 +1,23 @@
+# set environment variables
+export CIFS_SERVER=192.168.0.0                   # network server address to map to
+export CIFS_SHARE=Public/Pictures                # network share name to map to
+export MOUNT_POINT=/immich/pictures              # change only if required
+export CREDENTIALS_FILE=/immich/.credentials     # change only if required
+export OWNER_NAME=immich                         # change only if required 
+export OWNER_GROUP_NAME=immich                   # change only if required
+
+# create user
+useradd -m ${OWNER_NAME}
+
+# create folders
+mkdir /${OWNER_NAME} /${OWNER_NAME}/books /${OWNER_NAME}/db /${OWNER_NAME}/config
+
+# map drives
+../tools/map-drive.sh
+
+# download files
+cp metadata.db /${OWNER_NAME}/db
+cp docker-compose.yml /${OWNER_NAME}
+
+# reset permissions
+chown -R $(id -u ${OWNER_NAME}):$(id -g ${OWNER_NAME}) /${OWNER_NAME}
