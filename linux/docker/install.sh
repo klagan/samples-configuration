44bb3a5fc3dbd8f9ee11ebdb54aa90247cd58968
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-28 14:50:35 +0100

    Update install.sh

--- linux/docker/install.sh
+++ linux/docker/install.sh
@@ -9,5 +9,5 @@ sudo curl -fsSL https://get.docker.com | sh
 docker version
 docker compose version
 
-# add self to docker group
+# message to user: add self to docker group
 echo -e "\n\e[1;31mAdd self to docker:\e[0m\e[1;32m sudo usermod -aG docker $USER\e[0m"
