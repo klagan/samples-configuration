61610e9df8f254db482d6d71f493d9993ed26236
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-07-30 11:55:17 +0100

    Update install.sh

--- linux/calibre-web/install.sh
+++ linux/calibre-web/install.sh
@@ -1,23 +1,23 @@
 # set environment variables
-export CIFS_SERVER=192.168.0.0                       # network server address to map to
-export CIFS_SHARE=Public/Books                       # network share name to map to
-export MOUNT_POINT=/calibreweb/books                 # change only if required
-export CREDENTIALS_FILE=/calibreweb/.credentials     # change only if required
-export OWNER_NAME=calibreweb                         # change only if required 
-export OWNER_GROUP_NAME=calibreweb                   # change only if required
+export CIFS_SERVER=192.168.0.0                         # network server address to map to
+export CIFS_SHARE=Public/Books                         # network share name to map to
+export MOUNT_POINT=/opt/calibreweb/books               # change only if required
+export CREDENTIALS_FILE=/opt/calibreweb/.credentials   # change only if required
+export OWNER_NAME=calibreweb                           # change only if required 
+export OWNER_GROUP_NAME=calibreweb                     # change only if required
 
 # create user
 useradd -m ${OWNER_NAME}
 
 # create folders
-mkdir /${OWNER_NAME} /${OWNER_NAME}/books /${OWNER_NAME}/db /${OWNER_NAME}/config
+mkdir /opt/calibreweb /opt/calibreweb/books /opt/calibreweb/db /opt/calibreweb/config
 
 # map drives
 ../tools/map-drive.sh
 
 # download files
-cp metadata.db /${OWNER_NAME}/db
-cp docker-compose.yml /${OWNER_NAME}
+cp metadata.db /opt/calibreweb/db
+cp docker-compose.yml /opt/calibreweb
 
 # reset permissions
-chown -R $(id -u ${OWNER_NAME}):$(id -g ${OWNER_NAME}) /${OWNER_NAME}
+chown -R $(id -u ${OWNER_NAME}):$(id -g ${OWNER_NAME}) /opt/calibreweb
