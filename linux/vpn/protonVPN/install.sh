801c9c4da8b53360f93100e5f39d4db02211bc8c
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-08-08 18:40:43 +0100

    Update install.sh

--- linux/vpn/protonVPN/install.sh
+++ linux/vpn/protonVPN/install.sh
@@ -1,9 +1,6 @@
 
-## EDIT VARIABLES HERE <----------------------------------------------------------------------
-export vpn_username=                              # your vpn username
-export vpn_password=                              # your vpn password
-export credentials_file=.vpn_credentials          # change only if you need a different name
-export ovpn_filename=./ch.protonvpn.udp.ovpn  # change this to your own openvpn file
+# get variables from ./.env file
+source .env
 
 # install packages
 apt -y update && apt -y upgrade
@@ -14,14 +11,14 @@ apt autoremove
 wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
 chmod +x "/etc/openvpn/update-resolv-conf"
 
-echo "${vpn_username}" | tee "${credentials_file}" > /dev/null
-echo "${vpn_password}" | tee -a "${credentials_file}" > /dev/null
+echo "$vpn_username" | tee "$vpn_credentials_file" > /dev/null
+echo "$vpn_password" | tee -a "$vpn_credentials_file" > /dev/null
 
-mv ${credentials_file} /etc/openvpn
-chmod 600 /etc/openvpn/${credentials_file}
+mv $vpn_credentials_file /etc/openvpn
+chmod 600 /etc/openvpn/$vpn_credentials_file
 
 # configure vpn
-cp ${ovpn_filename} /etc/openvpn/myvpn.conf
+cp $vpn_ovpn_filename /etc/openvpn/myvpn.conf
 
 # include credentials file
 sed -i 's/^auth-user-pass$/auth-user-pass \/etc\/openvpn\/.vpn_credentials/' /etc/openvpn/myvpn.conf
