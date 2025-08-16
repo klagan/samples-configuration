2fd701a7a4ad5da91d8c23030f661bdb5b475249
Kam Lagan <244992+klagan@users.noreply.github.com>
2025-08-16 11:53:16 +0100

    Update 01-install-baseline.sh

--- linux/01-install-baseline.sh
+++ linux/01-install-baseline.sh
@@ -6,16 +6,5 @@ apt autoremove
 # install pi apps appstore
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
-# set up swap space (if not already configured)
-fallocate -l 1G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
-# optimize system settings
-sysctl -w vm.swappiness=10
-sysctl -w vm.vfs_cache_pressure=50
-
 # verify installations
 systemctl list-units --type=service --state=running
